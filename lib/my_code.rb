adfa3513008d1619a20b9317cc4777d4182a793f
Christina Park <christinapark0117@gmail.com>
2019-09-14 05:10:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# Your Code Here
+def map(source_array)
+  index = 0 
+  new_array = []
+  while index < source_array.length do
+    new_array.push(yield(source_array[index]))
+    index += 1
+  end
+  return new_array
+end
+
+def reduce()
